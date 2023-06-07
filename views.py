b49368b33d8159dce3c7aae36949ab8ae596fcf2
Keerthanaushar <120699074+Keerthanaushar@users.noreply.github.com>
2023-06-07 12:23:33 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import product
 def home(request):
-    return HttpResponse("Hello World")
+    product = product.object.all()
+  return render(request,'index1.html',{'product':product})
 
 def contact(request):
     return HttpResponse("This is contact page")
